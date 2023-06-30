422a27e521d4968597993b435732012e1a0748d3
gowtham <gowthamsgobi@gmail.com>
2023-06-30 21:28:14 +0530

    upto array method concept workouted

--- src/concepts/array-methods.js
+++ src/concepts/array-methods.js
@@ -69,3 +69,60 @@ console.log(
     return a - b;
   })
 );
+
+// -- The most importent method slice and splice method
+// slice method is used to crate a copy of an array or
+// return a portion in an existing array.
+
+// syntax: slice( optional start parameter, optional end parameter ) == both are index value only
+
+// 1 without optional start and end parameter
+
+const food1 = ["Itly", "rice", "biriyani", "chicken grevy", "fish fry"];
+const op1 = food1.slice();
+console.log("slice method - without optional start and end parameters", op1);
+
+// 2 with optional start paramter plus value , nagative value and end parameter also
+const food2 = ["Itly", "rice", "biriyani", "chicken grevy", "fish fry"];
+const op2 = food2.slice(2);
+const op3 = food2.slice(-3);
+const op4 = food2.slice(5);
+const op5 = food2.slice(2, 4);
+
+console.log("slice method - with optional start  parameter", op2);
+console.log("slice method - with optional negative parameter", op3);
+console.log("slice method - with optional greater than the last index", op4);
+console.log(
+  "slice method - The end position will not be included in the extracted elements for the new array.",
+  op5
+);
+
+// -- splice method
+// Unlike the slice() method,
+// the splice() method will change the contents of the original array.
+// The splice() method is used to add or remove elements of an existing array
+// and the return value will be the removed items from the array.
+
+// syntax:
+
+// splice(start, optional delete count, optional items to add), == start optional parameter only index value
+
+const food3 = ["pizza", "cake", "salad", "cookie"];
+
+console.log("splice method ", food3);
+
+const op6 = food3.splice(1, 0, "burrito");
+
+console.log(
+  "splice method - with optional start parameter and new items",
+  food3
+);
+console.log("splice method - if i return this value this is the output", op6);
+
+const op7 = food3.splice(2, 2, "New food 1", "New food 2");
+
+console.log(
+  "splice method - with optional start paramater and delete count  value",
+  food3
+);
+console.log("splice method - if i return this value this is the output", op7);
